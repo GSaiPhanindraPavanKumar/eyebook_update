eea3bd7a1c5e2a723744772226020a1ab3918089
Sumanth <gsumanth101@gmail.com>
2024-10-17 23:02:13 +0530

    first commit

---
+++ config/connection.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully or already exists";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ views/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Welcome to Eyebook</h1>";
+echo "<p>This is the landing page.</p>";
+?>
