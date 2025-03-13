19a11b7a5abf944029f17eab62ee5748b463b282
GSaiPhanindraPavanKumar <saiphanindra2004@gmail.com>
2025-03-13 16:02:42 +0530

    student assessment

--- views/student/view_assessments.php
+++ views/student/view_assessments.php
@@ -68,6 +68,8 @@ while ($row = $stmt->fetch()) {
     <?php include 'footer.html'; ?>
 </div>
 
+<!-- Include jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
 function showCompletedMessage(score) {
